5510fa985d6b484c4bb580c1c0e8f09458c5a7e1
Anusha Reddy <areddy7@uncc.edu>
2020-11-18 20:50:34 -0500

    Added Timer

--- app/src/main/java/com/example/plato/Pomodoro.java
+++ app/src/main/java/com/example/plato/Pomodoro.java
@@ -4,9 +4,16 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +26,13 @@ public class Pomodoro extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    //variables for the timer
+    private TextView countdownText;
+    private Button countdownButton;
+    private CountDownTimer countDownTimer;
+    private long timeLeft = (60000 * 25);
+    private boolean isTimerRunning;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -53,12 +67,73 @@ public class Pomodoro extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_pomodoro, container, false);
+        countdownText = (TextView) view.findViewById(R.id.countdown_text);
+        countdownButton = (Button) view.findViewById(R.id.start_button);
+        countdownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startStop();
+
+            }
+        });
+        updateTimer();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pomodoro, container, false);
+        return view;
+    }
+
+    public void startStop() {
+        if (isTimerRunning) {
+            stopTimer();
+        } else
+            startTimer();
+    }
+
+    public void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeft, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeft = millisUntilFinished;
+                updateTimer();
+//                String sDuration=String.format(Locale.ENGLISH, "%02d: %02d"
+//                , TimeUnit.MICROSECONDS.toMinutes(1)
+//                , TimeUnit.MILLISECONDS.toSeconds(1)-
+//                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)));
+//                countdownText.setText(sDuration);
+
+            }
+
+            @Override
+            public void onFinish() {
+                countdownText.setText("Timer Ended :D");
+//                Toast.makeText(getApplicationContext(), "Countdown Timer has ended", Toast.LENGTH_LONG).show();
+
+            }
+        }.start();
+        countdownButton.setText("Pause");
+        isTimerRunning = true;
+
+    }
+
+    public void stopTimer() {
+        countDownTimer.cancel();
+        isTimerRunning = false;
+        countdownButton.setText("Stop");
+    }
+
+    public void updateTimer() {
+        int mins = (int) (timeLeft / 60000);
+        // int seconds=(int)  mins%60000/1000;
+        String timeLeftText = mins + "mins";
+//        if(seconds<10)
+//            timeLeftText+="0";
+//        timeLeftText+=seconds;
+        countdownText.setText(timeLeftText);
     }
 }
